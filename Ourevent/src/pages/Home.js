81f087d9f10dad42fce6230efc3d2a2b9a82a11d
Kauarafaldini <84752774+Kauarafaldini@users.noreply.github.com>
2022-08-09 23:45:46 -0300

    Add files via upload

---
+++ Ourevent/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+export default Home(){
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+
---
+++ Ourevent/src/pages/Sobre.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const Sobre = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Sobre;
